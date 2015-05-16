ba93e2583c9db7bb013b0b49978c6a6b19e0a0b0
David Rodriguez Fuentes <davidrf09@gmail.com>
2015-05-16 13:44:47 -0400

    Implement Activity

---
+++ app/models/activity.rb
@@ -0,0 +1,21 @@
+class Activity
+  ACCESS_TOKEN ="QHuOkULLbTtply8RYc8LtbIeeXFH"
+  BASE_URL = "https://api.nike.com/v1/me/sport"
+  IMPORTANT_NUMBER = "1431792350948"
+  OPTIONS = { headers: { 'Accept' => 'application/json' } }
+
+  def self.all
+    path = "/activities"
+    query = "?access_token=#{ACCESS_TOKEN}&_=#{IMPORTANT_NUMBER}"
+    url = BASE_URL + path + query
+    binding.pry
+    HTTParty.get(url, OPTIONS).parsed_response
+  end
+
+  def self.find(id)
+    path = "/activities/#{id}"
+    query = "?access_token=#{ACCESS_TOKEN}&_=#{IMPORTANT_NUMBER}"
+    url = BASE_URL + path + query
+    HTTParty.get(url, OPTIONS).parsed_response
+  end
+end
