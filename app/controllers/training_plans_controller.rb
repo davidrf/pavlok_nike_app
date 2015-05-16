b052c5e6f24fbc4cd13e703738a8e3caa2c3d515
Joshua Mun <joshmun23@gmail.com>
2015-05-16 15:31:11 -0400

    added new form w/ controllers for training plan

---
+++ app/controllers/training_plans_controller.rb
@@ -0,0 +1,48 @@
+class TrainingPlansController < ApplicationController
+  def index
+    @user = current_user
+    @training_plan = @user.training_plans.new
+    @times = []
+    year, month, day = Date.today.strftime('%Y %m %d').split
+    time = Time.new(year.to_i, month.to_i, day.to_i)
+
+    48.times do
+      @times << time
+      time += 1800
+    end
+
+    @times.map! {|e| [e.strftime('%H:%M'), e]}[0]
+    @display_time = []
+    @stored_time = []
+
+    @times.each_with_index do |arr, index|
+      @stored_time = @times[1]
+
+    end
+    # binding.pry
+  end
+
+  def new
+
+  end
+
+  def create
+    @user = current_user
+    @training_plan = @user.training_plans.new(training_plans_params)
+    binding.pry
+    if @training_plan.save
+
+      redirect_to user_training_plans_path
+    else
+      flash[:errors] = @training_plan.errors
+      render :new
+    end
+  end
+
+
+  private
+
+  def training_plans_params
+    params.require(:training_plan).permit(:plan_name, :start_time, :end_time)
+  end
+end
